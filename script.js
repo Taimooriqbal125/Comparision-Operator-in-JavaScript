234def6e7986dae57a0b51d12e43d12063126aef
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-05-08 03:16:32 -0700

    Comparison

---
+++ script.js
@@ -0,0 +1,13 @@
+//  Comparison Operators in JavaScript
+
+// Less Than <
+ var num = 10;
+ var num2 = 15;
+ var Result = num < num2;
+ console.log("Comparision" , Result);
+
+//  Greater Than
+var dig = 11;
+var dig1 = 12;
+var result = dig1>dig;
+console.log(result) ;
