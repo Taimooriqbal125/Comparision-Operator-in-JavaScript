f46c352dfb0f380f70b6aa830ccdc76fd3c22fbd
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-07-05 06:32:21 -0700

    Comparision operator

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ var dig = 11;
 var dig1 = 12;
 var result = dig1>dig;
 console.log(result) ;
+
+// Less than Equal to 
+var temi = 16;
+var temi1 = 18;
+var result2 = temi <= temi1;
+console.log(result2);
+
+// Greater than equal to 
+var tem = 12;
+var tom = 14;
+var result3 = tem>=tom;
+console.log( "result" , result3);
